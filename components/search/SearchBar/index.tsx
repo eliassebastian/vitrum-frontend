85c077a765f0e00369e2697b7c928fb138b1af17
Elias Sebastian <eeschmoelz@gmail.com>
2022-10-18 14:13:10 +0100

    fix: search view on touch screen devices

--- components/search/SearchBar/index.tsx
+++ components/search/SearchBar/index.tsx
@@ -3,13 +3,26 @@ import {useRef, useState} from "react";
 import SearchBarInput from "../SearchBarInput";
 
 const SearchBar = () => {
+  const ref = useRef(null);
+  const [focused, setFocused] = useState(false);
+
+  const onFocus = () => {
+    if (!ref.current || !window.matchMedia("(hover:none), (pointer:coarse)").matches) return
+    setFocused(true);
+  }
+
+  const onBlur = () => {
+    if (!focused) return
+    setFocused(false);
+  }
+
   return (
     <div className={styles.container}>
       <form action={"/search"} role={"search"} method={"GET"}>
         <div>
-          <div className={styles.inner__container}>
-            <div className={styles.input__wrapper}>
-              <SearchBarInput/>
+          <div ref={ref} className={`${styles.inner__container} ${focused ? styles.inner__container__focus : ''}`}>
+            <div className={`${styles.input__wrapper} ${focused ? styles.input__wrapper__focus : ''}`}>
+              <SearchBarInput onBlur={onBlur} onFocus={onFocus} focused={focused}/>
             </div>
           </div>
         </div>
