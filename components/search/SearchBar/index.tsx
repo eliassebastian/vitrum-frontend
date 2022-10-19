a5ef286c7780ffe98461e1641916e206bb1701dc
Elias Sebastian <eeschmoelz@gmail.com>
2022-10-19 22:19:45 +0100

    fix: redundant components removed + unused parameters removed

--- components/search/SearchBar/index.tsx
+++ components/search/SearchBar/index.tsx
@@ -1,37 +1,120 @@
-import styles from "./SearchBar.module.scss"
-import {useRef, useState} from "react";
-import SearchBarInput from "../SearchBarInput";
+import styles from "./SearchBar.module.scss";
+import {FormEvent, useRef, useState} from "react";
 
-const SearchBar = () => {
-  const ref = useRef<HTMLDivElement>(null);
+const SearchBarNew = () => {
   const [focused, setFocused] = useState(false);
+  const [isEmpty, setEmptyState] = useState(true);
+  const ref = useRef<HTMLInputElement>(null);
 
-  const onFocus = () => {
-    if (!ref.current || !window.matchMedia("(hover:none), (pointer:coarse)").matches) return
+  const handleChange = (event: FormEvent<HTMLInputElement>) => {
+    const value = event.currentTarget.value;
+
+    if (value.length > 0 && isEmpty) {
+      setEmptyState(false)
+      return
+    }
+
+    if (value.length === 0 && !isEmpty) {
+      setEmptyState(true)
+      return
+    }
+  }
+
+  const onFocus = (event: FormEvent) => {
+    if (!ref.current || focused || window.matchMedia("(pointer: fine), (hover: hover)").matches) return
+    event.preventDefault();
+    event.stopPropagation();
+
     setFocused(true);
+    ref.current.focus({preventScroll: true});
+    window.scrollTo(0,0);
   }
 
   const onBlur = () => {
-    if (!focused) return
+    if (!ref.current || !focused) return
     setFocused(false);
+    ref.current.blur();
+  }
+
+  const clearValue = () => {
+    if (!ref.current) return;
+    setEmptyState(true);
+    ref.current.focus({preventScroll: true});
+    ref.current.value = "";
   }
 
   return (
-    <div className={styles.container}>
-      <form action={"/search"} role={"search"} method={"GET"} className={styles.form}>
-        <div>
-          <div ref={ref} className={`${focused ? styles.inner__container__focus : styles.inner__container}`}>
-            <div className={`${styles.input__wrapper} ${focused ? styles.input__wrapper__focus : ''}`}>
-              <SearchBarInput onBlur={onBlur} onFocus={onFocus} focused={focused}/>
-              <section style={{display: !focused ? "none" : "block"}} className={styles.section__mobile}>
+    <form className={`${ focused ? styles.form__focused : styles.form }`} action={"/search"} role={"search"} method={"GET"}>
+      <div className={`${ focused ? styles.wrapper__focused : styles.wrapper }`}>
+        {
+          !focused &&
+          <button
+            className={styles.left__button}
+            aria-label={"Search"}
+            type={"button"}
+            onClick={onFocus}
+          >
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <path d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"/>
+              </svg>
+            </span>
+          </button>
+        }
 
-              </section>
-            </div>
-          </div>
-        </div>
-      </form>
-    </div>
+        {
+          focused &&
+          <button
+            className={styles.left__button}
+            aria-label={"Back"}
+            type={"button"}
+            onClick={onBlur}
+          >
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <path d="M17 10a.75.75 0 01-.75.75H5.612l4.158 3.96a.75.75 0 11-1.04 1.08l-5.5-5.25a.75.75 0 010-1.08l5.5-5.25a.75.75 0 111.04 1.08L5.612 9.25H16.25A.75.75 0 0117 10z"/>
+              </svg>
+            </span>
+          </button>
+        }
+
+        <input
+          ref={ref}
+          className={styles.input}
+          onInvalid={(event) => {event.preventDefault()}}
+          required={true}
+          name={"q"}
+          type={"text"}
+          autoComplete={"off"}
+          autoCorrect={"off"}
+          placeholder={"Reddit URL"}
+          spellCheck={"false"}
+          onChange={handleChange}
+          onPointerDown={onFocus}
+        />
+
+        {
+          !isEmpty &&
+          <button
+            className={styles.clear__button}
+            aria-label={"Clear Search"}
+            type={"button"}
+            onClick={clearValue}
+          >
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z"/>
+              </svg>
+            </span>
+          </button>
+        }
+      </div>
+      {/*buttons - hide on focus*/}
+      <div>
+
+      </div>
+    </form>
   )
 }
 
-export default SearchBar
+export default SearchBarNew
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import SearchBar from "../components/search/SearchBar";
-import SearchBarNew from "../components/search/SearchBarNew";
+import SearchBarNew from "../components/search/SearchBar";
 import HeaderHome from "../components/header/HeaderHome";
 import FooterHome from "../components/footer/FooterHome";
 
