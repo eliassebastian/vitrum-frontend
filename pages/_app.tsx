babd5ee017d1d3e0c29efa7f6bae91be22a63400
Elias Sebastian <eeschmoelz@gmail.com>
2022-10-20 13:38:50 +0100

    feat: per page layouts enabled

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { ReactElement, ReactNode } from 'react';
+import type { AppProps } from 'next/app';
+import type { NextPage } from 'next';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
 }
 
-export default MyApp
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page)
+  return getLayout(<Component {...pageProps} />)
+}
