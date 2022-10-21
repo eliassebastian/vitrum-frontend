8c3ad7e2fada62c5478c3085cd92b695d9a2614b
Elias Sebastian <eeschmoelz@gmail.com>
2022-10-22 00:23:41 +0100

    feat: react query client provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { ReactElement, ReactNode } from 'react';
 import type { AppProps } from 'next/app';
 import type { NextPage } from 'next';
 import '../styles/globals.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
@@ -12,7 +13,13 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout
 }
 
+const client =  new QueryClient();
+
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-  const getLayout = Component.getLayout ?? ((page) => page)
-  return getLayout(<Component {...pageProps} />)
+  const getLayout = Component.getLayout ?? ((page) => page);
+  return getLayout(
+    <QueryClientProvider client={client}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  )
 }
