a813a9c9a5da23530a40a5bc05f3ffc4bd44d233
Elias Sebastian <eeschmoelz@gmail.com>
2022-10-26 15:07:53 +0100

    feat: empty search placeholder component

---
+++ components/search/SearchDefault/index.tsx
@@ -0,0 +1,10 @@
+
+import styles from "./SearchDefault.module.scss";
+
+const SearchDefault = () => {
+    return (
+        <h2>Start a search with a reddit link!</h2>
+    )
+}
+
+export default SearchDefault
