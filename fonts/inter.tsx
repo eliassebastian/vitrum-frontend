b197bfcf3630fca791239a13222240a802dee445
Elias Sebastian <eeschmoelz@gmail.com>
2022-11-20 00:00:33 +0000

    fix: variable font subset

--- fonts/inter.tsx
+++ fonts/inter.tsx
@@ -1,3 +1,3 @@
 import { Inter } from '@next/font/google';
 
-export const inter = Inter({variable: '--font-inter', axes: ["slnt"]});
+export const inter = Inter({variable: '--font-inter', axes: ["slnt"], subsets: ['latin']});
