716b59cb8730f07a01368179cd3e5e7d13d20c69
Elias Sebastian <eeschmoelz@gmail.com>
2022-11-25 16:48:27 +0000

    fix: use footer component

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import type { ReactNode } from "react";
+import Footer from "./Footer";
 import DefaultHeader from "./Header";
 import styles from "./style.module.scss";
 
@@ -10,26 +11,7 @@ export default function DefaultLayout({children}: {children: ReactNode}) {
             <main className={styles.main}>
                 {children}
             </main>
-            <footer className={styles.footer}>
-                <div className={styles.footer__firstrow}>
-                    <div className={styles.container}>
-                        <Link href={"/about"}>
-                            How Vitrum Works
-                        </Link>
-                    </div>
-                    <div style={{display: "flex", gap: "1rem"}}>
-                        <Link href={"/privacy"}>
-                            Privacy
-                        </Link>
-                        <Link href={"/Terms"}>
-                            Terms
-                        </Link>
-                    </div>
-                </div>
-                <div className={styles.footer__secondrow}>
-                    <h4>Made with &#10084;&#65039; in Edinburgh</h4>
-                </div>
-            </footer>
+            <Footer/>
         </div>
     )
 }
