87e2b993d306f27e600773f02d61d8fbf9dc8af7
Elias Sebastian <eeschmoelz@gmail.com>
2022-12-23 01:49:14 +0000

    feat: new search bar component with query type

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,11 +1,20 @@
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
 import SearchBar from "../../components/search/SearchBar";
+import SearchBarNew from "../../components/search/SearchBarNew";
 import styles from "./Home.module.scss";
+
+// const DynamicSearchBar = dynamic(() => import("../../components/search/SearchBar"), {
+//     suspense: true,
+// })
 
 export default function Page() {
     return (
         <section className={styles.container}>
             <div className={styles.wrapper}>
-                <SearchBar/>
+                <h2>VITRUM</h2>
+                {/* <SearchBar/> */}
+                <SearchBarNew/>
             </div>
         </section>
     )
