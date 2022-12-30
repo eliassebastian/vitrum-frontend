fee99e357cffadaeeadbf6e7cdbbf99d8e386f47
Elias Sebastian <eeschmoelz@gmail.com>
2022-12-30 16:23:00 +0000

    feat: mentions percentage change

--- app/(search)/trending/TrendingTableRowItem/index.tsx
+++ app/(search)/trending/TrendingTableRowItem/index.tsx
@@ -5,10 +5,8 @@ import styles from "./TrendingTableRowItem.module.scss";
 
 const TrendingTableRowItem = (props: TrendingItem) => {
     const mentionsMovement = !!props.mentions_24h_ago ? Math.round(((+props.mentions - +props.mentions_24h_ago) / +props.mentions_24h_ago) * 100) : null;
+    const mentionsPosNeg = mentionsMovement ? Math.sign(mentionsMovement) : "-";
     //console.log("mentionsMovement", mentionsMovement)
-
-    const rankMovement = !!props.rank_24h_ago ? (+props.rank_24h_ago - props.rank) : null
-    //console.log(props.rank, "rankMovement", rankMovement, +props.rank_24h_ago)
 
     const decoded = unescape(props.name);
 
@@ -20,7 +18,11 @@ const TrendingTableRowItem = (props: TrendingItem) => {
             <td className={styles.center}>{props.rank}</td>
             <td>{decoded}</td>
             <td className={styles.mobile}>{props.ticker}</td>
-            <td className={styles.center}>{mentionsMovement || "-"}</td>
+            <td className={styles.center}>
+                {
+                    mentionsPosNeg === "-" ? <span>-</span> : <span className={`${ mentionsPosNeg > 0 ? styles.up : styles.down }`}>{mentionsMovement}%</span>
+                }
+            </td>
             <td className={`${styles.mobile} ${styles.center}`}>{props.upvotes}</td>
         </tr>
     )
